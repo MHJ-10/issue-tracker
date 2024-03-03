143011e36c01491914f4b4520023a3461a4beaee
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-03-03 20:56:19 +0330

    add issues table

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,39 @@
+import prisma from '@/prisma/client';
 import Link from 'next/link';
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+
+  console.log(issues);
+
   return (
     <div>
       <Link className='btn-blue' href='/issues/new'>
         New Issue Page
       </Link>
+
+      <table className='my-10 w-full rounded-md border  text-left'>
+        <thead className='border-b font-medium dark:border-neutral-500'>
+          <tr>
+            <th className='px-6 py-4'>#</th>
+            <th className='px-6 py-4'>Title</th>
+            <th className='px-6 py-4'>Status</th>
+            <th className='px-6 py-4'>Created At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {issues.map((issue, i) => (
+            <tr className='border-b dark:border-neutral-500'>
+              <td className='whitespace-nowrap px-6 py-4'>{i + 1}</td>
+              <td className='whitespace-nowrap px-6 py-4'>{issue.title}</td>
+              <td className='whitespace-nowrap px-6 py-4'>{issue.status}</td>
+              <td className='whitespace-nowrap px-6 py-4'>
+                {issue.createdAt.toDateString()}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
