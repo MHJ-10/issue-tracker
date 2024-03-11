d1abc26314257fad0621e7af44f2747896d55dc4
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-03-11 20:51:51 +0330

    change style

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -3,9 +3,9 @@ import { Issue } from '@prisma/client';
 
 const IssueDetails = ({ issue }: { issue: Issue }) => {
   return (
-    <div>
+    <div className='flex flex-col items-start gap-3'>
       <p className='text-3xl'>{issue?.title}</p>
-      <div className='flex gap-3'>
+      <div className='flex gap-5'>
         <IssueStatusBadge status={issue.status} />
         <p>{issue?.createdAt.toDateString()}</p>
       </div>
