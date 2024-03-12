0f77286f92342d0e7d3c706a87865375ad3a169e
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-03-12 12:05:55 +0330

    seperate validation file

---
+++ app/validationSchema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const issueSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: 'Title is required' })
+    .max(255, { message: 'Title must be at most 255 characters' }),
+  description: z.string().min(1, { message: 'Description is required.' }),
+});
