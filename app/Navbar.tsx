852ba18c458838904dc00a1e7dd0e1b96021886d
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-03-16 14:04:36 +0330

    refactor Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -14,9 +14,18 @@ interface ILink {
 }
 
 const Navbar = () => {
-  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  return (
+    <nav className='bg-slate-200 px-3 py-4'>
+      <div className='container mx-auto flex items-center justify-between'>
+        <NavLinks />
+        <AuthDropdown />
+      </div>
+    </nav>
+  );
+};
+
+const NavLinks = () => {
   const pathname = usePathname();
-  const { data, status } = useSession();
 
   const links: ILink[] = [
     { label: 'Dashboard', href: '/' },
@@ -24,55 +33,66 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className='bg-slate-200 px-3 py-4'>
-      <div className='container mx-auto flex items-center justify-between'>
-        <div className='flex items-center justify-start gap-7'>
-          <Link href='/'>
-            <FaBug size={26} />
-          </Link>
-          <ul className='flex gap-5'>
-            {links.map((link) => (
-              <li key={link.href}>
-                <Link
-                  className={`${link.href === pathname && 'text-zinc-950 underline underline-offset-2'} text-zinc-600`}
-                  href={link.href}
-                >
-                  {link.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          {status === 'authenticated' && (
-            <Dropdown
-              root={
-                <button onClick={() => setShowDropdown((prev) => !prev)}>
-                  <Image
-                    className='inline-block h-8 w-8 rounded-full ring-2 ring-white'
-                    src={data.user?.image!}
-                    width={24}
-                    height={24}
-                    quality={75}
-                    alt={data.user?.name!}
-                  />
-                </button>
-              }
-              showDropdown={showDropdown}
+    <div className='flex items-center justify-start gap-7'>
+      <Link href='/'>
+        <FaBug size={26} />
+      </Link>
+      <ul className='flex gap-5'>
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className={`${link.href === pathname && 'text-zinc-950 underline underline-offset-2'} text-zinc-600`}
+              href={link.href}
             >
-              <div className='flex flex-col p-2 gap-2'>
-                <p>{data.user?.name}</p>
-                <p>{data.user?.email}</p>
-                <Link className='pt-2 border-t border-slate-200' href='/api/auth/signout'>Logout</Link>
-              </div>
-            </Dropdown>
-          )}
-          {status === 'unauthenticated' && (
-            <Link href='/api/auth/sigin'>Login</Link>
-          )}
-        </div>
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const AuthDropdown = () => {
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  const { data, status } = useSession();
+
+  if (status === 'loading') return null;
+
+  if (status === 'unauthenticated')
+    return (
+      <Link className='text-zinc-600' href='/api/auth/sigin'>
+        Login
+      </Link>
+    );
+
+  return (
+    <Dropdown
+      root={
+        <button onClick={() => setShowDropdown((prev) => !prev)}>
+          <Image
+            className='inline-block h-8 w-8 rounded-full ring-2 ring-white'
+            src={data!.user?.image!}
+            width={24}
+            height={24}
+            quality={75}
+            alt={data!.user?.name!}
+          />
+        </button>
+      }
+      showDropdown={showDropdown}
+    >
+      <div className='flex flex-col gap-2 p-2'>
+        <p>{data!.user?.name}</p>
+        <p>{data!.user?.email}</p>
+        <Link
+          className='border-t border-slate-200 pt-2'
+          href='/api/auth/signout'
+        >
+          Logout
+        </Link>
       </div>
-    </nav>
+    </Dropdown>
   );
 };
 
