f7e7d64d3f319437563d8db6ca25b4713798fc0b
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-03-21 23:18:57 +0330

    add patchIssueSchema

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const issueSchema = z.object({
   title: z
@@ -6,4 +6,21 @@ export const issueSchema = z.object({
     .min(1, { message: 'Title is required' })
     .max(255, { message: 'Title must be at most 255 characters' }),
   description: z.string().min(1, { message: 'Description is required.' }),
+});
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: 'Title is required' })
+    .max(255, { message: 'Title must be at most 255 characters' })
+    .optional(),
+  description: z
+    .string()
+    .min(1, { message: 'Description is required.' })
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, { message: 'Assigned to user id id required.' })
+    .optional()
+    .nullable(),
 });
