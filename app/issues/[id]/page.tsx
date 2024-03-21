a56e18bb609ebc1ab33dd0a374ae94e2f9fb1f23
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-03-21 15:19:09 +0330

    add fetch users

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -5,6 +5,7 @@ import { notFound } from 'next/navigation';
 import DeleteIssueButton from '../_components/DeleteIssueButton';
 import EditIssueButton from '../_components/EditIssueButton';
 import IssueDetails from '../_components/IssueDetails';
+import AssigneeSelect from '../_components/AssigneeSelect';
 
 interface Props {
   params: {
@@ -23,12 +24,13 @@ const IssueDetailsPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <div className='my-2 grid grid-rows-2 gap-4 md:grid-cols-2 md:gap-8'>
+    <div className='my-2 grid grid-rows-2 gap-4 px-3 sm:px-0 md:grid-cols-2 md:gap-8'>
       <div>
         <IssueDetails issue={issue} />
       </div>
       {session && (
         <div className='flex flex-col gap-5'>
+          <AssigneeSelect />
           <EditIssueButton issueId={issue.id} />
           <DeleteIssueButton issueId={issue.id} />
         </div>
---
+++ app/issues/_components/AssigneeSelect.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { httpService } from '@/services/httpService';
+import { User } from '@prisma/client';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { BsChevronDown } from 'react-icons/bs';
+
+const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [selectedUser, setSelectedUser] = useState<User>();
+  const [showSelect, setShowSelect] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await httpService.get<User[]>('/users');
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className='w-full sm:w-1/2'>
+      <label
+        id='listbox-label'
+        className='block text-sm font-medium leading-6 text-gray-900'
+      >
+        Assigned to
+      </label>
+      <div className='relative mt-2'>
+        <button
+          type='button'
+          className='relative flex w-full items-center justify-between rounded-md bg-white px-2 py-1  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-slate-300 sm:text-sm sm:leading-6'
+          onClick={() => setShowSelect((prev) => !prev)}
+        >
+          <span>{selectedUser?.name ?? 'Select a user'}</span>
+          <BsChevronDown
+            className={`${showSelect && 'rotate-180'} transition-all duration-300`}
+          />
+        </button>
+
+        {showSelect && (
+          <ul className='absolute z-10 mt-1 max-h-56 w-full rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+            {users.map((user) => (
+              <li className='relative  select-none py-2  text-gray-900'>
+                <div
+                  className='flex items-center justify-start gap-2 ps-2'
+                  onClick={() => {
+                    setShowSelect(false);
+                    setSelectedUser(user);
+                  }}
+                >
+                  <Image
+                    className='rounded-full'
+                    src={user.image!}
+                    alt={user.name!}
+                    width={25}
+                    height={25}
+                  />
+                  <span className='ml-3 block truncate font-normal'>
+                    {user.name}
+                  </span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AssigneeSelect;
