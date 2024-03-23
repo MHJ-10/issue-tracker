abcadcf02abdd91b86de4c19715798a398c366ca
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-03-23 14:00:58 +0330

    add export  components

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,2 +1,6 @@
-export { default as Skeleton } from './Skeleton';
+export { default as Dialog } from './Dialog';
+export { default as Dropdown } from './Dropdown';
 export { default as IssueStatusBadge } from './IssueStatusBadge';
+export { default as Select } from './Select';
+export { default as Skeleton } from './Skeleton';
+
