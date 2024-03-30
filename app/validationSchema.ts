031d6d2618ca8a3def9ab6f57d1d680b8d91d68e
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-03-30 14:19:28 +0330

    add form validation

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -5,7 +5,7 @@ export const issueSchema = z.object({
     .string()
     .min(1, { message: 'Title is required' })
     .max(255, { message: 'Title must be at most 255 characters' }),
-  description: z.string().min(1, { message: 'Description is required.' }),
+  description: z.string().min(1, { message: 'Description is required' }),
 });
 
 export const patchIssueSchema = z.object({
@@ -16,11 +16,37 @@ export const patchIssueSchema = z.object({
     .optional(),
   description: z
     .string()
-    .min(1, { message: 'Description is required.' })
+    .min(1, { message: 'Description is required' })
     .optional(),
   assignedToUserId: z
     .string()
-    .min(1, { message: 'Assigned to user id id required.' })
+    .min(1, { message: 'Assigned to user id id required' })
     .optional()
     .nullable(),
 });
+
+export const loginSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters' })
+    .max(20, { message: 'Password must be at most 20 characters' }),
+});
+
+export const registerSchema = loginSchema
+  .extend({
+    name: z
+      .string()
+      .min(1, { message: 'Name is required' })
+      .max(40, { message: 'Password must be at most 40 characters' }),
+    confirmPassword: z.string(),
+  })
+  .superRefine(({ password, confirmPassword }, c) => {
+    if (password !== confirmPassword) {
+      c.addIssue({
+        code: 'custom',
+        message: 'The passwords did not match',
+        path: ['confirmPassword'],
+      });
+    }
+  });
