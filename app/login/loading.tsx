2d9badb312a195a29a4d8047bf768dd7916fbc78
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2024-03-31 14:51:41 +0330

    implement form loading

---
+++ app/login/loading.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Skeleton } from '../components';
+
+const LoginLoadingPage = () => {
+  return (
+    <div className='mx-auto flex w-full flex-col justify-center rounded-md border border-slate-200 p-2 sm:w-3/4 md:w-2/3 '>
+      <p className='mx-auto my-2 w-1/3'>
+        <Skeleton height={32} />
+      </p>
+
+      <Skeleton className='form-input my-2' height={55} count={2} />
+
+      <div className='mx-auto inline-block w-2/3 rounded py-3 sm:w-1/3'>
+        <Skeleton height={40} />
+      </div>
+
+      <p className='mx-auto mt-2 w-2/3 sm:w-1/3'>
+        <Skeleton height={22} />
+      </p>
+    </div>
+  );
+};
+
+export default LoginLoadingPage;
