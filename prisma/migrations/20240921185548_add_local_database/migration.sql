905e6835eed063b890282ad3c24c6a3ed795c0d2
MHJ-10 <mhj10mhj10jafari@gmail.com>
2024-09-21 22:37:53 +0330

    add a migration

--- prisma/migrations/20240921185548_add_local_database/migration.sql
+++ prisma/migrations/20240921185548_add_local_database/migration.sql
@@ -47,6 +47,7 @@ CREATE TABLE `User` (
     `name` VARCHAR(191) NULL,
     `email` VARCHAR(191) NULL,
     `emailVerified` DATETIME(3) NULL,
+    `password` VARCHAR(191) NOT NULL,
     `image` VARCHAR(191) NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
