5303764f5b80637c8bbc71974d7b4bd2e29e9791
MHJ-10 <mhj10mhj10jafari@gmail.com>
2024-09-24 10:58:12 +0330

    create useCloseElement hook

---
+++ app/hooks/useCloseElement.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+
+export const useCloseElement = <T extends HTMLElement>(
+  onCallback: () => void
+) => {
+  const ref = useRef<T>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      onCallback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return ref;
+};
